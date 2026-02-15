2304100bb4bb51bb214debea7a8b2456d70906ed
Amr Aboelela <amraboelela@gmail.com>
2026-02-15 14:06:47 -0800

    enhancements

--- AutoRefreshExtension/background.js
+++ AutoRefreshExtension/background.js
@@ -1,23 +1,37 @@
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'checkChanges') {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'showNotification') {
+        // Create a Chrome notification for new jobs
+        const notificationOptions = {
+            type: 'basic',
+            iconUrl: 'icon.png',
+            title: request.title,
+            message: request.message,
+            priority: 2
+        };
+
+        chrome.notifications.create('newJobsNotification', notificationOptions, (notificationId) => {
+            if (chrome.runtime.lastError) {
+                console.error('Notification error:', chrome.runtime.lastError);
+            } else {
+                console.log('Notification created:', notificationId);
+            }
+        });
+    } else if (request.action === 'checkChanges') {
         const currentTime = new Date();
         console.log('checkChanges, Current time:', currentTime);
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            // You can loop through tabs and send a message to each one
-            tabs.forEach(function(tab) {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            tabs.forEach((tab) => {
                 chrome.tabs.sendMessage(tab.id, { action: 'refresh' });
             });
         });
-        chrome.tabs.query({ active: false, currentWindow: true }, function(tabs) {
-            // You can loop through tabs and send a message to each one
-            tabs.forEach(function(tab) {
+        chrome.tabs.query({ active: false, currentWindow: true }, (tabs) => {
+            tabs.forEach((tab) => {
                 chrome.tabs.sendMessage(tab.id, { action: 'refresh' });
             });
         });
-        chrome.tabs.query({ active: false, currentWindow: false }, function(tabs) {
-            // You can loop through tabs and send a message to each one
-            tabs.forEach(function(tab) {
+        chrome.tabs.query({ active: false, currentWindow: false }, (tabs) => {
+            tabs.forEach((tab) => {
                 chrome.tabs.sendMessage(tab.id, { action: 'refresh' });
             });
         });
