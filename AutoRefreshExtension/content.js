1dfb8ec2390c199aea884afe406b2a8069cedf1f
Amr Aboelela <amraboelela@users.noreply.github.com>
2026-05-04 12:37:53 -0700

    between 5 and 15

--- AutoRefreshExtension/content.js
+++ AutoRefreshExtension/content.js
@@ -228,7 +228,7 @@ if (hostname.includes('linkedin.com') || hostname.includes('indeed.com')) {
 } else {
     checkChanges();
 }
-const minutes = Math.floor(Math.random() * 5) + 3; // between 2 and 5 minutes
+const minutes = Math.floor(Math.random() * 11) + 5; // between 5 and 15 minutes
 
 setTimeout(() => {
     console.log("#refresh setTimeout - preparing to refresh");
